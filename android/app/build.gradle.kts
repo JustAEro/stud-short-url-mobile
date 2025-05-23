2ee824f002265b6019698cb6c09bb0414763a078
ilyapolukhin <ilya.polukhin46@gmail.com>
2025-05-23 15:31:35 +0300

    feat: bump ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.stud_short_url_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
